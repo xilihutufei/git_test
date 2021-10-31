4c5668e96a57db2be0799fd2293ba3a5231a320b
puhongfei <puhongfei@58.com>
2021-10-31 21:25:24 +0800

    第一次提交

---
+++ src/main/java/shuai/fei/Test.java
@@ -0,0 +1,9 @@
+package shuai.fei;
+
+/**
+ * @Auth puhongfei
+ * @Date 2021/10/31
+ * @Desc TODO
+ */
+public class Test {
+}
---
+++ src/main/java/shuai/fei/entity/GitTest.java
@@ -0,0 +1,58 @@
+package shuai.fei.entity;
+
+import java.util.List;
+
+/**
+ * @Auth puhongfei
+ * @Date 2021/10/31
+ * @Desc TODO
+ */
+public class GitTest {
+
+    private String gitName;
+    private int gitNum;
+    private Double getPrise;
+    private List<Integer> gits;
+
+    public GitTest() {
+    }
+
+    public GitTest(String gitName, int gitNum, Double getPrise, List<Integer> gits) {
+        this.gitName = gitName;
+        this.gitNum = gitNum;
+        this.getPrise = getPrise;
+        this.gits = gits;
+    }
+
+    public String getGitName() {
+        return gitName;
+    }
+
+    public void setGitName(String gitName) {
+        this.gitName = gitName;
+    }
+
+    public int getGitNum() {
+        return gitNum;
+    }
+
+    public void setGitNum(int gitNum) {
+        this.gitNum = gitNum;
+    }
+
+    public Double getGetPrise() {
+        return getPrise;
+    }
+
+    public void setGetPrise(Double getPrise) {
+        this.getPrise = getPrise;
+    }
+
+    public List<Integer> getGits() {
+        return gits;
+    }
+
+    public void setGits(List<Integer> gits) {
+        this.gits = gits;
+    }
+}
